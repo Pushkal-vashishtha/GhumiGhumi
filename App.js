6463be0e921c792e2b769cef9a33b679fc78c829
Pushkal-vashishtha <pushkalvas730@gmail.com>
2024-05-22 15:05:38 +0530

    initial

---
+++ App.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/routes/AppNavigator';
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppNavigator/>
+    </NavigationContainer>
+  )
+}
+
+export default App
